a475f4c41f9897c6ddc1a37f4f89722e903aa8fb
Israel Moreira <israel_moreira2009@yahoo.com.br>
2024-08-06 22:49:14 -0300

    saida com try catch

--- ControleFinanceiro.API/Controllers/SaidaController.cs
+++ ControleFinanceiro.API/Controllers/SaidaController.cs
@@ -5,32 +5,39 @@ using static ControleFinanceiro.Application.DTOs.SaidaDTO;
 
 namespace ControleFinanceiro.API.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class SaidaController : ControllerBase
-	{
-		private readonly ISaidaService _saidaService;
-		public SaidaController(ISaidaService saidaService)
-		{
-			_saidaService = saidaService;
-		}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaidaController : ControllerBase
+    {
+        private readonly ISaidaService _saidaService;
+        public SaidaController(ISaidaService saidaService)
+        {
+            _saidaService = saidaService;
+        }
 
-		[HttpGet("RetornaSaida")]
-		public IActionResult RetornaSaida(Guid id)
-		{
-			return Ok(_saidaService.RetornaSaidaPorId(id).Result);
-		}
+        [HttpGet("RetornaSaida")]
+        public IActionResult RetornaSaida(Guid id)
+        {
+            return Ok(_saidaService.RetornaSaidaPorId(id).Result);
+        }
 
-		[HttpGet("RetornaTodos")]
-		public IActionResult RetornaTodos()
-		{
-			return Ok(_saidaService.RetornaTodosSaida().Result);
-		}
+        [HttpGet("RetornaTodos")]
+        public IActionResult RetornaTodos()
+        {
+            return Ok(_saidaService.RetornaTodosSaida().Result);
+        }
 
-		[HttpPost]
-		public IActionResult AdicionarSaida(AdicionarSaidaDTO adicionarSaidaDTO)
-		{
-			return Ok(_saidaService.AdicionarSaida(adicionarSaidaDTO).Result);
-		}
-	}
+        [HttpPost]
+        public IActionResult AdicionarSaida(AdicionarSaidaDTO adicionarSaidaDTO)
+        {
+            try
+            {
+                return Ok(_saidaService.AdicionarSaida(adicionarSaidaDTO).Result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
 }
